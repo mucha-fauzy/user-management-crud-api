6b3632ae82ad01f1740aa02ccaf8fa6aad150d1e
mucha-fauzy <muchamad.fauzy@evermos.com>
2023-08-10 08:48:03 +0700

    add status and profile id must be crated first than user

--- seeders/domain/auth/auth_seed.go
+++ seeders/domain/auth/auth_seed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 
 	"github.com/evermos/boilerplate-go/configs"
@@ -11,8 +12,26 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type Profile struct {
+	ProfileID string    `db:"profile_id"`
+	CreatedAt time.Time `db:"created_at"`
+	CreatedBy string    `db:"created_by"`
+	UpdatedAt time.Time `db:"updated_at"`
+	UpdatedBy string    `db:"updated_by"`
+}
+
+type Status struct {
+	StatusID  string    `db:"status_id"`
+	CreatedAt time.Time `db:"created_at"`
+	CreatedBy string    `db:"created_by"`
+	UpdatedAt time.Time `db:"updated_at"`
+	UpdatedBy string    `db:"updated_by"`
+}
+
 type User struct {
 	ID        string    `db:"id"`
+	ProfileID string    `db:"profile_id"`
+	StatusID  string    `db:"status_id"`
 	Username  string    `db:"username"`
 	Password  string    `db:"password"`
 	Role      string    `db:"role"`
@@ -27,7 +46,9 @@ func main() {
 
 	mysqlConn := infras.ProvideMySQLConn(config)
 
-	uuid := "ce560f71-1495-4d6c-b6bf-f9f1b3b2f212"
+	id := uuid.New().String()
+	profileID := uuid.New().String()
+	statusID := uuid.New().String()
 	username := "admin_fauzy"
 	password := "passwordkuat"
 	// Generate the bcrypt hash for the password
@@ -39,8 +60,27 @@ func main() {
 	role := "admin"
 	CreatedAt := time.Now()
 	updatedAt := time.Now()
+
+	profile := Profile{
+		ProfileID: profileID,
+		CreatedAt: CreatedAt,
+		CreatedBy: username,
+		UpdatedAt: updatedAt,
+		UpdatedBy: username,
+	}
+
+	status := Status{
+		StatusID:  statusID,
+		CreatedAt: CreatedAt,
+		CreatedBy: username,
+		UpdatedAt: updatedAt,
+		UpdatedBy: username,
+	}
+
 	user := User{
-		ID:        uuid,
+		ID:        id,
+		ProfileID: profileID,
+		StatusID:  statusID,
 		Username:  username,
 		Password:  string(hashedPassword),
 		Role:      role,
@@ -50,11 +90,43 @@ func main() {
 		UpdatedBy: username,
 	}
 
-	// Insert the user data into the database
-	query := "INSERT INTO ums_users (id, username, password, role, created_at, created_by, updated_at, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+	// Insert profile_id into ums_profiles
+	profileQuery := "INSERT INTO ums_profiles (id, created_at, created_by, updated_at, updated_by) VALUES (?,?,?,?,?)"
 	_, err = mysqlConn.Write.Exec(
-		query,
+		profileQuery,
+		profile.ProfileID,
+		profile.CreatedAt,
+		profile.CreatedBy,
+		profile.UpdatedAt,
+		profile.UpdatedBy,
+	)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to insert profile data")
+		return
+	}
+
+	// Insert status_id into ums_status
+	statusQuery := "INSERT INTO ums_status (id, created_at, created_by, updated_at, updated_by) VALUES (?,?,?,?,?)"
+	_, err = mysqlConn.Write.Exec(
+		statusQuery,
+		status.StatusID,
+		status.CreatedAt,
+		status.CreatedBy,
+		status.UpdatedAt,
+		status.UpdatedBy,
+	)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to insert status data")
+		return
+	}
+
+	// Insert the user data into ums_users
+	userQuery := "INSERT INTO ums_users (id, profile_id, status_id, username, password, role, created_at, created_by, updated_at, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	_, err = mysqlConn.Write.Exec(
+		userQuery,
 		user.ID,
+		user.ProfileID,
+		user.StatusID,
 		user.Username,
 		user.Password,
 		user.Role,
@@ -64,7 +136,7 @@ func main() {
 		user.UpdatedBy,
 	)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to insert data")
+		log.Error().Err(err).Msg("Failed to insert user data")
 		return
 	}
 }
