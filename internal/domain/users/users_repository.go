e397d50298e99c82e86348896f0a2e538aefeb59
mucha-fauzy <muchamad.fauzy@evermos.com>
2023-08-10 17:05:56 +0700

    add method for update profile and delete user by id

--- internal/domain/users/users_repository.go
+++ internal/domain/users/users_repository.go
@@ -1,6 +1,11 @@
 package users
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/evermos/boilerplate-go/infras"
 	"github.com/rs/zerolog/log"
 )
@@ -8,7 +13,9 @@ import (
 type UserRepository interface {
 	GetData(filter UserFilter, page, size int) ([]UserView, error)
 	CountTotalData(filter UserFilter) (int, error)
-	GetProfile(uid string) (*ProfileView, error)
+	GetProfile(uuid string) (*ProfileView, error)
+	UpdateProfile(uuid string, profile *UpdateProfile) (*UpdateProfile, error)
+	DeleteUserByID(uuid string) error
 }
 
 type UserRepositoryMySQL struct {
@@ -210,7 +217,32 @@ func (r *UserRepositoryMySQL) CountTotalData(filter UserFilter) (int, error) {
 	return totalData, nil
 }
 
-func (r *UserRepositoryMySQL) GetProfile(uid string) (*ProfileView, error) {
+func (r *UserRepositoryMySQL) DeleteUserByID(uuid string) error {
+	query := `
+		DELETE FROM ums_users
+		WHERE id = ? AND role != 'admin'
+	`
+
+	result, err := r.DB.Write.Exec(query, uuid)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to delete user")
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to check affected rows")
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return errors.New("User with 'admin' role cannot be deleted or user not found")
+	}
+
+	return nil
+}
+
+func (r *UserRepositoryMySQL) GetProfile(uuid string) (*ProfileView, error) {
 	query := `
 	SELECT 
 		p.name,
@@ -244,10 +276,68 @@ func (r *UserRepositoryMySQL) GetProfile(uid string) (*ProfileView, error) {
 	`
 
 	var profile ProfileView
-	err := r.DB.Read.Get(&profile, query, uid)
+	err := r.DB.Read.Get(&profile, query, uuid)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to get profile")
 		return nil, err
 	}
 	return &profile, nil
+}
+
+func (r *UserRepositoryMySQL) UpdateProfile(uuid string, profile *UpdateProfile) (*UpdateProfile, error) {
+	// Build the SET clause for the update query
+	setClauses := []string{
+		"p.name = COALESCE(?, p.name)",
+		"p.gender = COALESCE(?, p.gender)",
+		"p.dob = COALESCE(?, p.dob)",
+		"p.education = COALESCE(?, p.education)",
+		"p.address = COALESCE(?, p.address)",
+		"p.city = COALESCE(?, p.city)",
+		"p.province = COALESCE(?, p.province)",
+		"p.phone_number = COALESCE(?, p.phone_number)",
+		"p.updated_at = ?, p.updated_by = ?",
+		"u.updated_at = ?, u.updated_by = ?",
+	}
+
+	// Construct the SQL query
+	query := fmt.Sprintf(`
+		UPDATE ums_profiles AS p
+		INNER JOIN ums_users AS u ON p.id = u.profile_id
+		SET %s
+		WHERE u.id = ?`,
+		strings.Join(setClauses, ", "))
+
+	profile.UpdatedAt = time.Now()
+	values := []interface{}{
+		lowercaseOrNil(profile.Name),
+		lowercaseOrNil(profile.Gender),
+		profile.DoB,
+		lowercaseOrNil(profile.Education),
+		lowercaseOrNil(profile.Address),
+		lowercaseOrNil(profile.City),
+		lowercaseOrNil(profile.Province),
+		profile.PhoneNumber,
+		profile.UpdatedAt,
+		profile.UpdatedBy,
+		profile.UpdatedAt,
+		profile.UpdatedBy,
+		uuid,
+	}
+
+	// Execute the update query
+	_, err := r.DB.Write.Exec(query, values...)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to update profile")
+		return nil, err
+	}
+
+	return profile, nil
+}
+
+// lowercaseOrNil converts a string pointer to lowercase if it's not nil.
+func lowercaseOrNil(s *string) interface{} {
+	if s != nil {
+		return strings.ToLower(*s)
+	}
+	return nil
 }
